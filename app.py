f2a0654097fb21aebf8ed65cec0c7fc93af9720a
crashRT <crashrt.farbauti@gmail.com>
2023-02-05 02:22:10 +0900

    SQLAlchemy

--- app.py
+++ app.py
@@ -4,15 +4,30 @@ from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
-
 logging.basicConfig(level=logging.DEBUG)
-
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+conn = sa.create_engine('sqlite:///db.sqlite3')
+
+Base = declarative_base()
+class OhuroRecords(Base):
+    id = sa.Column(sa.Integer, primary_key=True)
+    user = sa.Column(sa.String)
+    date = sa.Column(sa.Date)
+    def __init__(self, user, date):
+        self.user = user
+        self.date = date
+    def __repr__(self):
+        return "<OhuroRecords('%s', '%s')>" % (self.user, self.date)
+
 
 @app.message("おふろチャレンジ成功")
 def message_clear(message, say):
+    user = message["user"]
     say("えらいにゃ！！")
 
 @app.message("おふろチャレンジ失敗")
