5a7970dd1675424744dcab3448c8d50119b4923d
crashRT <crashrt.farbauti@gmail.com>
2023-02-05 02:05:44 +0900

    反応

--- app.py
+++ app.py
@@ -3,8 +3,26 @@ import os
 from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(level=logging.DEBUG)
 
-app = App(token=os.environ["SLACK_BOT_SOCKET_TOKEN"])
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+@app.message("おふろチャレンジ成功")
+def message_clear(message, say):
+    say("えらいにゃ！！")
+
+@app.message("おふろチャレンジ失敗")
+def message_fail(message, say):
+    say("がんばれにゃ！！")
+
+@app.message("にゃーん")
+def message_nyan(message, say):
+    say("にゃーん")
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
