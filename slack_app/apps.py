56eeccffa1f7b1149f5f991185fbfcd0431844e1
crashRT <crashrt.farbauti@gmail.com>
2023-02-05 01:22:33 +0900

    やり直し

--- slack_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SlackAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'slack_app'
--- slack_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('oauth/', views.oauth, name='slack_oauth'),
-    path('events/', views.Events.as_view(), name='events0'),
-]
